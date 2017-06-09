bfe81b46da4c15958efce1a8b60470b12ae9b74f
dzhonapp <dzhonapp@gmail.com>
2017-06-09 15:52:55 -0700

    fahrenheit to celcius

---
+++ Celsius to Fahreneit Temperature Converter.py
@@ -0,0 +1,11 @@
+'''9. Celsius to Fahrenheit Temperature Converter
+Write a program that converts Celsius temperatures to Fahrenheit temperatures. The formula
+is as follows:
+F =9/5C+32
+
+The program should ask the user to enter a temperature in Celsius, and then display the
+temperature converted to Fahrenheit.
+'''
+
+fahreneit= int(input('What is todays Temperature in Fahrenheit? '))
+print('Todays temperature in Celcius: ', (fahreneit-32)/(9/5))
