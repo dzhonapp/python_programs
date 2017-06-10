f8dca04dcab8babcb1bd1fbd92d73a4ccb05aeb1
dzhonapp <dzhonapp@gmail.com>
2017-06-09 18:47:06 -0700

    new files

---
+++ areasRectangle.py
@@ -0,0 +1,9 @@
+'''2. Areas of Rectangles
+The area of a rectangle is the rectangle’s length times its width. Write a program that asks
+for the length and width of two rectangles. The program should tell the user which rectangle
+has the greater area, or if the areas are the same.'''
+
+length = float(input('What is the lenght of rectange? '))
+width = float(input('What is the width of rectangle? '))
+print('Area of rectangle is: ')
+print(length*width)
