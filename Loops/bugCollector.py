5cbb0b2da809a78853210b1e1bb4b39a511cd82f
dzhonapp <dzhonapp@gmail.com>
2017-06-12 03:54:48 -0700

    Loops

--- Loops/bugCollector.py
+++ Loops/bugCollector.py
@@ -5,3 +5,14 @@ total of the number of bugs collected during the five days. The loop should ask 
 number of bugs collected for each day, and when the loop is finished, the program should
 display the total number of bugs collected.
 '''
+
+days=int(input("How many days? "))
+
+numberOfBugs=int(input('How many bugs are collected per day! '))
+
+total_bugs=0;
+
+for day in range(days):
+    total_bugs+=numberOfBugs
+
+print(total_bugs)
