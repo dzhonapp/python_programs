e4a5dece545340d33082260f6ceb1514d4276490
dzhonapp <dzhonapp@gmail.com>
2017-06-13 05:26:05 -0700

    Loops

--- Loops/budgetAnalysis.py
+++ Loops/budgetAnalysis.py
@@ -5,4 +5,17 @@ month and keep a running total. When the loop finishes, the program should displ
 amount that the user is over or under budget.'''
 
 amountBudgeted=float(input('What is the amount you budgeted for a month? '))
+tempAmountBudgeted=amountBudgeted
+flag = 0
+while flag<5:
+    expense = float(input('Write you expenses! '))
+    amountBudgeted-=expense
+    print(amountBudgeted)
+    flag+=1
+print('Your budget was: ', tempAmountBudgeted)
+if amountBudgeted<0:
+    print("You spend more than your budget for: ", "-",(tempAmountBudgeted-amountBudgeted)-amountBudgeted)
+else:
+    print('You spend less than your budget and saved: ', amountBudgeted)
+
 
