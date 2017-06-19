e631604017aea93178a0d34b0eb317983702f646
dzhonapp <dzhonapp@gmail.com>
2017-06-18 20:45:46 -0500

    Loops

--- Loops/tuitionIncrease.py
+++ Loops/tuitionIncrease.py
@@ -5,3 +5,9 @@ announced that the tuition will increase by 3 percent each year for the next 5 y
 a program with a loop that displays the projected semester tuition amount for the next 5
 years.
 '''
+tuition =8000;
+year=5
+print('First year tution: ', tuition)
+for i in range(year):
+    tuition +=tuition*0.03
+    print("Next year tutition:", format(tuition, ",.2f"))
