869b80b80062004ee896fac96b1575e9f7f66283
dzhonapp <dzhonapp@gmail.com>
2017-06-25 21:40:35 -0500

    Loops

--- Loops/challengingquestions.py
+++ Loops/challengingquestions.py
@@ -1,14 +1,13 @@
 import copy
 
-A=[[13,4,8,14,1],[9,6,3,7,21], [5,12,17,9,3]]
+A=[[13,4,8,14,1],[9,6,3,7,21], [5,12,17,9,3],[5,12,17,9,3],[5,12,17,9,3]]
+
 B=[]
 
 for i in range(len(A[0])):
-    print("\n")
-    B.append(list(""))
+    B.append(list())
     for y in range(len(A)):
-        B.append(list(""))
-        print(B)
+        B[i].append(A[y][i])
 
 
-
+print(B)
