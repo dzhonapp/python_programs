fefda35faf08661fc1c4e23ab62a938e97a78864
dzhonapp <dzhonapp@gmail.com>
2017-10-06 15:24:05 -0500

    Functions

--- functions/randomNumberGussingGame.py
+++ functions/randomNumberGussingGame.py
@@ -1,22 +1,29 @@
 import random
 flag = True
 
-user_guess = []
 
+count = 0
+play_won = 0
 while flag:
+    user_guess = []
     numberRandom = random.randint(1, 10)
     for i in range(3):
         user_input = int(input("Guess the number! "))
         user_guess.append(user_input)
-
+    count = count + 1
     if numberRandom in user_guess:
-        print("You got it!" , "Random Number was: " , numberRandom)
+        play_won = play_won +1
+        print("Whao, You got it!" , "Random Number was: " , numberRandom)
     else:
         print("Sorry buddy you have to play again, wanna play? " + "Random Number was: " , numberRandom)
 
     play = input("Y/N")
     if play.upper() == "Y":
         continue
+    elif play.upper() =="N":
+        flag = False
+        print("You played ", " times! and won from them just! ", play_won, "times!")
     else:
         flag = False
+
 print(user_guess)
